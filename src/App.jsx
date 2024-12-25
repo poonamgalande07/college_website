d60e7748aed77caa42a79bb8a088c7248f3473c8
poonamgalande07 <punamgalande27@gmail.com>
2024-12-25 07:24:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Banner from './Component/Carosule/Banner'
+import Footer from './Component/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Banner/>
+
+
+
+      
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
